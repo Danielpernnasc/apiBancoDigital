cd11d0125f21a0d72a4e5af554be5d598d5619d6
Danielpernnasc <daniel.nascimento@smartezy.com.br>
2025-07-10 16:22:28 -0300

    atualizando

--- src/main/java/com/api/apiBanco/config/CorsConfig.java
+++ src/main/java/com/api/apiBanco/config/CorsConfig.java
@@ -10,10 +10,9 @@ public class CorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
-            @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "http://18.217.92.231")
+                        .allowedOrigins("http://localhost:4200", "http://3.142.166.243")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("Authorization", "Content-Type", "Accept")
                         .allowCredentials(true);
